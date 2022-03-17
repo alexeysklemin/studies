f1b03828ab7655ad238c6019c7046ad54022c89b
alexeysklemin <a76sklemin@gmail.com>
2022-03-17 19:28:31 +0400

    init commit

--- seabatle.cpp
+++ seabatle.cpp
@@ -1,59 +1,18 @@
 #include <iostream>
 
-bool CheckingParams (int coordinates[2][2]){
+bool CheckCoodinates (int xb, int xf, int yb, int yf);
 
-	if (((coordX0!=coordX1)&&(coordY0!=coordY1))||((coordX0>9)&&(coordX1>9))||((coordY0>9)&&(coordY1>9))){
-		return false;
-	}
-return true;
-}
-
-void  InputingCoordSubmarine (int coordinates[2][2]){
-
-	int i=0;
-	
-	while(i<4){
-		std::cout<<"Input coordinates of  "<<i<<" submarin"<<"\n";
-		std::cin>>coordinates[0][0]>>coordinates[0][1];
-		
-		for(int j=0; j<2; ++j){
-			for(int k=0; k<2; ++k){
-				coordinates[j][k]=coordinates[0][0];
-			}
-		}
-	
-	}
-++i;
-}
-	
-
-	for(int i=0; i<4; ++i)void InstallingShips(bool installShips [10][20][1], , int coordinates[2][2], bool player){
-
-	int i = 0;
-	int j = 0;
-	
-	if(!CheckingParams){
-		
-
-	if(player){
-	j=+10;
-	}
-
-	
-
-void KillingShips(bool installShips [10][20][1], bool player);
-
+bool CheckEngeged (int xb, int xf, int yb, int yf);
+ 
 
 int main (){
 
-	bool installShips [10][20][1];
-	bool  player;
-	int coordinates[2][2]
+	bool installShips [10][10];
+	bool fireCoordinates [10][10];
+	int ships [4] = {1,2,3,4};
+	int  xb, xf, yb, yf;
 
 	
-	player = false;
-	InstallingShips(installShips);
-	player = true;
-	InstallingShips(installShips);
+	
 
 }
