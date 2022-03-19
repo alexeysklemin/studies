d4b6f3f395f0bd82c642c9e5fe56462c6d1db495
alexeysklemin <a76sklemin@gmail.com>
2022-03-19 19:43:55 +0400

    Engeged function

--- seabatle.cpp
+++ seabatle.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-bool CheckCoodinates (int xb, int xf, int yb, int yf){
+bool CheckCoordinates (int xb, int xf, int yb, int yf){
 
 	if((xb<0)||(xf<0)||(yb<0)||(yf<0)||(xb>9)||(xf>9)||(yb>9)||(yf>9)){
 		return false;
@@ -23,27 +23,36 @@ bool CheckEngeged (int xb, int xf, int yb, int yf, bool installShips[10][10]){
 	if(x==0){
 		b=yb;
 		f=yf;
+		for(int i=f; i>b; --i){
+			if(installShips[xb][i]==true){
+				std::cout<<"You can't put ship here. This point is engeged";
+				return false;
+			}
+		}
 	}else{
 		if(y==0){
 			b=xb;
 			f=xf;
+			for(int i1=f; i1>b; --i1){
+				if(installShips[i1][yb]==true){
+					std::cout<<"You can't put ship here. This point is engeged";
+					return false;
+				}
+			}
 		}
 	}
 	
-	for(int i=f; i<b; --i){
-		
-		if(installShips[i]==true){
-			std::cout<< "You can't put ship here";
-			return false;
-		}
-	}
+
 	return true;
 } 
 
+
 int main (){
 
-	bool installShips [10][10];
-	bool fireCoordinates [10][10];
+	bool installShipsPlayerA  [10][10];
+	bool installShipPlayerB [10][10];
+	bool fireCoordinatesPlayerA [10][10];
+	bool fireCoordinatesPlayerB [10[10];
 	int ships [4] = {1,2,3,4};
 	int  xb, xf, yb, yf;
 
