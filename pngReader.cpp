e37f8828dfa4100868240dbae96def398254694d
alexeysklemin <a76sklemin@gmail.com>
2022-09-12 20:58:54 +0400

    new mistakes

--- pngReader.cpp
+++ pngReader.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include <cstring>
 
 int main(){
-	char exp[1024];
+	char fileRead[1024];
+	char filename[9];
 
 
-	std::string filename = "liza.png";
+	filename = "liza.png";
 	std::cout<<"Input your file's path ";
 //	std::cin>>filename;
-	std::string searchString;
+	//std::string searchString;
 	std::ifstream file;
 	file.open(filename, std::ios::binary);
 
@@ -18,15 +19,23 @@ int main(){
 		std::cout<<"File is not found";
 		std::cout<<"\n";
 	}else{
-		file.getline(exp, 1024, '\0');
-		char* expPtr = &exp;
-		while(exp!='\0'){
-			if(exp!='P'){
-				expPtr++;
-			}
-			if(exp!='N'){
-				expPtr++;
-			}
+		file.getline(fileRead, 1024, '\0');
+		char* expPtr = &fileRead;
+		expPtr++;
+        if(*(expPtr+1) == 'P' && *(expPtr+2) == 'N' && *(expPtr+3) =='G'){
+            bool fil = true;
+        }
+        expPtr = &filename;
+        expPtr = strlen(filename);
+        if(*expPtr == 'g' && *(expPtr-1) == 'n' && *(expPtr-2) == 'p' && *(expPtr-3) =='.'){
+            bool name = true;
+        }
+        if(fil && name){
+            std::cout<<"It is *.png File";
+        }else{
+            std::cout<<"It is not *.png file";
+        }
+            
 
 	}
 
