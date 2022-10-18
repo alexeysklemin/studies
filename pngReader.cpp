c6208d4d6f76ab7f1e6e423f159ae9ee3fe7408a
alexeysklemin <a76sklemin@gmail.com>
2022-10-18 21:31:27 +0400

    final pngReader

--- pngReader.cpp
+++ pngReader.cpp
@@ -8,10 +8,10 @@ int main(){
 	std::string  filename;
 
 
-	filename = "liza.png";
-//	std::cout<<"Input your file's path ";
-//	std::cin>>filename;
-	//std::string searchString;
+//	filename = "liza.png";
+	std::cout<<"Input your file's path ";
+	std::cin>>filename;
+	
 	bool fil = false;
 	bool name = false;
 	int i = 0;
@@ -24,19 +24,19 @@ int main(){
 	}else{
 		file.getline(fileRead, 8, '\0');
 		char* expPtr = fileRead;
-		//expPtr++;
+		
 	std::cout<<*(expPtr+3)<<"\n"; 
 	std::cout<<(int)*expPtr<<"\t"<<*(expPtr+1)<<"\t"<<*(expPtr+2)<<"\t"<<*(expPtr+3)<<"\n";
         if(*(expPtr+1) == 'P' && *(expPtr+2) == 'N' && *(expPtr+3) =='G' && (int)*expPtr == -119){
            fil = true;
         }
-	//std::cout<<fil<<"\n";
+	
         int rfil = filename.rfind(".png");
-//	 std::cout<<rfil<<"\n";
+
         if(rfil<5){
             name = true;
         }
-	std::cout<<name<<"\t"<<fil<<"\n";
+	
         if(fil && name){
             std::cout<<"It is *.png File\n";
         }else{
