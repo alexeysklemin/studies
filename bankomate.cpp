d88a86671b077111febbd78ae13c9de2e19204c1
alexeysklemin <a76sklemin@gmail.com>
2022-11-06 10:00:27 +0400

    init commit atm

---
+++ bankomate.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+
+
+void calculationBanknotes(){
+    
+}
+
+void defSumm(){
+    
+    
+}
+
+void addingBanknotes(){
+
+    
+}
+
+void withdrawingBanknotes(){
+    
+}
+
+
+
+int main(){
+
+    char action;
+    int sumFromUser=0;
+    char banknote[4];
+    std::string filename = "banknotes.bin";
+    
+    std::getline(std::cin, action);
+    
+    if(ction=='-'){
+        calculationBanknotes();
+        defSumm();
+        withdrawingBanknotes();
+    }else{
+        if(action=='+'){
+            calculationBanknotes();
+            defSumm();
+            addingBanknotes();
+        }
+    }
+    
+    
+    
+    
+}
