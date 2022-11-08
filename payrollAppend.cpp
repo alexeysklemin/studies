0f0a01e83dc4a6227c9cd18f846a6debe73ce390
alexeysklemin <a76sklemin@gmail.com>
2022-11-08 11:08:09 +0400

    input chages

--- payrollAppend.cpp
+++ payrollAppend.cpp
@@ -4,16 +4,32 @@
 
 int main(){
 
-	std::string firstPerson = "Tom Hank 34500 01.04.2019";
+	/*std::string firstPerson = "Tom Hank 34500 01.04.2019";
 	std::string secPerson = "Alice Woddecker 56000 09.09.2020";
 	std::string thirdPerson = "Woody Alen 45600 09.10.2921";
-	std::string forthPerson = "Alex Sklemin 34700 23.07.2022";
+	std::string forthPerson = "Alex Sklemin 34700 23.07.2022"*/;
+
+	std::string names;
+	std::string  salary;
+	std::string date;
+	std::string person;
+
+	while(true){
+	
+	person = " ";
+	std::getline(std::cin, names);
+	std::getline(std::cin, salary);
+	std::getline(std::cin, date);
+
+	person = names+' '+salary+' '+date;
 
 	std::ofstream out;
 	out.open("words.txt", std::ios::app);
 
 	if(out.is_open()){
 
-		out<<"\n"<<firstPerson<<"\n"<<secPerson<<"\n"<<thirdPerson<<"\n"<<forthPerson<<"\n";
+		out<<"\n"<<person<<"\n";
 	}
+	out.close();
+   }
 }
